ed7f4368da3ea3f74dce0e13ad93d9096c8cf59c
sacern <spbrittain@gmail.com>
2019-03-12 19:05:37 -0400

    Initial Check in

--- VidYo/App_Start/BundleConfig.cs
+++ VidYo/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace VidYo
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/Lumen-Bootstrap.css",
                       "~/Content/site.css"));
         }
     }
